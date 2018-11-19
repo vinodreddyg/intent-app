39608166190e1436654f6c6edfc005cf3c919fcf
Vinod Reddy <vinodreddyg28@gmail.com>
2018-11-19 23:51:45 +0530

    Minor Fix

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ MONGO_URI = "mongodb://localhost:27017/intentDb"
 DP_INTENT_CONFIG_PATH = './app/deeppavlov/config/intents_snips_big.json'
 DP_NER_CONFIG_PATH = './app/deeppavlov/config/ner_conll2003.json'
 
-SNIPS_MODEL_PATH = './app/snipsnlu/models/snips_sample'
+SNIPS_MODEL_PATH = './app/snipsnlu/models/snipsnlu-snips'
 
 RASA_MODEL_PATH = './app/rasanlu/models/chat'
